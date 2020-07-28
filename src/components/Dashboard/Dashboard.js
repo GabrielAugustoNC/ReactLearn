d8f73cbc653ca0aa83c9368ec1755b9a07882b2f
Gabriel Augusto Nunes Carneiro <Gabriel.Augusto@beal.net>
2020-07-28 16:51:59 -0300

    ajustes do menu principal

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,15 +2,9 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import { mainListItems, secondaryListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
@@ -98,10 +92,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Dashboard() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -207,32 +207,3 @@ export default function Menu() {
         </React.Fragment >
     );
 }
-
-
-    // return (<AppBar color="primary" position="static">
-
-
-    //     <Toolbar color="primary">
-    //         <img src={LogoImg} alt="" className="app-header__logo" />
-
-
-
-    //         <div className="app-menu-group">
-    //             <Link className="app-menu__link" to="/">
-    //                 <Fab className="app-menu__button" variant="extended" size="medium">Home</Fab>
-    //             </Link>
-    //             <Link className="app-menu__link" to="/formLogin">
-    //                 <Fab className="app-menu__button" variant="extended" size="medium">Novo Usuário</Fab>
-    //             </Link>
-    //             <Link className="app-menu__link" to="/login">
-    //                 <Fab className="app-menu__button" variant="extended" size="medium">Login</Fab>
-    //             </Link>
-    //             <Link className="app-menu__link" to="/charts">
-    //                 <Fab className="app-menu__button" variant="extended" size="medium">Gráficos</Fab>
-    //             </Link>
-    //             <Link className="app-menu__link" to="/dashboard">
-    //                 <Fab className="app-menu__button" variant="extended" size="medium">Dashboard</Fab>
-    //             </Link>
-    //         </div>
-    //     </Toolbar>
-    // </AppBar>);
